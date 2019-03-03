9acac32446bc53ed214e8c18ae5c1d88df164493
joshpitzalis <joshpitzalis@gmail.com>
2019-03-03 15:49:35 +0530

    refactor for maintainability

--- src/features/votes/__tests/votes.test.jsx
+++ src/features/votes/__tests/votes.test.jsx
@@ -1,39 +1,12 @@
 import 'jest-dom/extend-expect';
-import React from 'react';
-import {
-  cleanup,
-  fireEvent,
-  render,
-  waitForElement
-} from 'react-testing-library';
-import TaskProvider from '../../../context/TasksContext';
-import Tasks from '../index';
+import { cleanup } from 'react-testing-library';
 
 afterEach(cleanup);
 
-it.skip('lets me edit an existing tasks list', async () => {
-  const { queryByTestId, getByTestId, getByText } = render(
-    <TaskProvider>
-      <Tasks />
-    </TaskProvider>
-  );
-
-  await waitForElement(() => getByText('Thing List'));
-  expect(queryByTestId('taskListEditor')).toBeNull();
-  fireEvent.click(getByTestId('editTaskList'));
-  await waitForElement(() => getByTestId('taskListEditor'));
-  fireEvent.change(getByTestId('titleInput'), {
-    target: { value: 'Thingy List' }
-  });
-  fireEvent.change(getByTestId('taskInput'), {
-    target: { value: 'example task' }
-  });
-  fireEvent.click(getByTestId('addToDo'));
-  fireEvent.click(getByTestId('submitTodoList'));
-  expect(queryByTestId('taskListEditor')).toBeNull();
-  await waitForElement(() => getByText('Thingy List'));
-  getByText('example task');
-});
+it('lets me edit an existing vite list', () => {});
 
 it('add an option when creating a vote', () => {});
 it('lets me create a vote', () => {});
+
+it('lets me record a vote', () => {});
+it('people can change their vote before deadline', () => {});
