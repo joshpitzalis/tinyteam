a93e3abeb2ed63f1ac2a363a2bdbd87efce2458a
joshpitzalis <joshpitzalis@gmail.com>
2019-03-03 16:26:52 +0530

    now you can add an option to an active poll

--- src/features/votes/Poll.jsx
+++ src/features/votes/Poll.jsx
@@ -22,11 +22,26 @@ export const Poll = ({ poll: { id, title, deadline }, transition }) => {
     firestore.doc(`decisions/${id}`).delete();
   };
 
+  const [value, setValue] = React.useState('');
+
+  const submitNewOption = voteId => async e => {
+    e.preventDefault();
+
+    const newOption = await firestore
+      .collection(`decisions/${voteId}/options`)
+      .doc();
+    await firestore
+      .doc(`decisions/${voteId}/options/${newOption.id}`)
+      .set({ title: value, id: newOption.id });
+
+    setValue('');
+  };
+
   return (
     <section className="mw6-ns w-100 center tc ">
       <header>
         <h2 className="f1 lh-title">{title}</h2>
-        <h3>A little desctiption goes heres</h3>
+        {/* <h3>A little desctiption goes heres</h3> */}
       </header>
       <hr className="dn" />
       <form className="mt3">
@@ -37,24 +52,43 @@ export const Poll = ({ poll: { id, title, deadline }, transition }) => {
               const count = option.votes ? option.votes.length : 0;
               return (
                 <div key={option.id} className="pa2 ma0 tl bg-white">
-                  <label className=" tl ma0  ">
+                  <label className=" ma0 w-100 ">
                     <input
                       type="checkbox"
                       name="responses"
                       checked={voted}
                       onChange={() => handleChange(voted, option.id)}
                     />{' '}
-                    {option.title} <b className="f3">{count}</b>
+                    {option.title}
                     <span className="radiomark " />
                   </label>
+                  <b className="f3 fr">{count}</b>
                 </div>
               );
             })}
         </div>
-        <input type="submit" value={'Submit'} className="mt3" />
+        <input
+          type="submit"
+          value="Submit"
+          className="mt3 pa4 br3 ttu pointer b"
+        />
       </form>
       {/* <h3>{deadline} left...</h3> */}
-      <p className="red b pointer" onClick={() => deletePoll(id)}>
+
+      <form onSubmit={submitNewOption(id)}>
+        <input
+          type="text"
+          value={value}
+          className="db w-100 pa3 br3 ma3 mb0 mt6"
+          placeholder="Add a new option to the mix..."
+          onChange={e => setValue(e.target.value)}
+        />
+        <button type="submit" className="db w-100 pa3 br3 ma3 mt0">
+          Add An Option
+        </button>
+      </form>
+
+      <p className="washed-red b pointer" onClick={() => deletePoll(id)}>
         Delete this poll
       </p>
     </section>
